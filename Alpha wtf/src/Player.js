332f8192a56e110f5d447ba27b02825c2a1da4c7
Minewarpro <hugo.roche13@hotmail.com>
2022-04-20 19:32:45 +0200

    Update Player.js

--- Alpha wtf/src/Player.js
+++ Alpha wtf/src/Player.js
@@ -173,14 +173,18 @@ class Player {
                 this.player.setTexture('test_dash');
                 this.player.anims.stop();
 
-                let angle = 60 * Phaser.Math.Angle.Between(
+                let angle = Phaser.Math.Angle.Between(
                     me.player.body.x,
                     me.player.body.y,
-                    this.scene.game.input.mousePointer.x + this.scene.cameras.main.worldView.x,
-                    this.scene.game.input.mousePointer.y + this.scene.cameras.main.worldView.y,
+                    me.scene.game.input.mousePointer.x + me.scene.cameras.main.worldView.x,
+                    me.scene.game.input.mousePointer.y + me.scene.cameras.main.worldView.y,
                 );
                 console.log(angle);
-                this.player.setAngle(angle);
+                console.log(me.direction);
+                if (me.direction===-1){
+                    this.player.setFlipX(false);
+                }
+                this.player.setAngle(angle * 60);
 
                 console.log("start dash");
                     me.scene.physics.moveTo(
@@ -196,56 +200,12 @@ class Player {
                     me.isDashing = false;
                     me.fireBall.visible = false;
                     me.player.setAngle(0);
-                    me.player.anims.play('right',true);
+                    me.player.anims.play('fall',true);
+                    if (me.direction===-1){
+                        me.player.setFlipX(true);
+                    }
                     console.log("dash terminé");
                 }, 200)
-            }
-        }
-    }
-
-    dashFollow() {
-        let me = this;
-
-        if (this.dashIsUp){
-            if (this.flagDash){
-
-            } else {
-                me.player.setVelocityX(0);
-                me.player.setVelocityY(0);
-                this.dashIsUp = false;
-                this.flagDash = true;
-
-                console.log("start dash")
-                this.dashTween = this.scene.tweens.add({
-                    targets: this.player,
-                    speedFactor:'-=200',
-                    // alpha: { start: 0, to: 1 },
-                    // alpha: 1,
-                    // alpha: '+=1',
-                    ease: "Circ.easeOut", // 'Cubic', 'Elastic', 'Bounce', 'Back'
-                    duration: 270,
-                    onUpdate: function () {
-                        console.log(me.player.speedFactor);
-                        me.player.body.setAllowGravity(false);
-                        console.log("dash en cours");
-                        me.scene.physics.moveTo(
-                            me.player,
-                            me.scene.game.input.mousePointer.x + me.scene.cameras.main.worldView.x,
-                            me.scene.game.input.mousePointer.y + me.scene.cameras.main.worldView.y,
-                            me.player.speedFactor);
-                    },
-                    onComplete: function () {
-                        console.log("dash terminé");
-                        me.player.dashplay = false;
-                        me.player.speedFactor=900
-                        me.player.body.setAllowGravity(true);
-                        me.player.setVelocityX(me.player.body.velocity.x * 0.6);
-                        me.player.setVelocityY(me.player.body.velocity.y * 0.6);
-                    }
-                    //repeat: -1, // -1: infinity
-                    //yoyo: true
-                });
-
             }
         }
     }
@@ -323,6 +283,7 @@ class Player {
 
     moveRight(){
         this.player.setVelocityX(300);
+        this.direction=1;
         this.player.setFlipX(false);
         if (this.player.body.onFloor()) {
             //this.player.play('right', true)
@@ -346,6 +307,7 @@ class Player {
 
     moveLeft(){
         this.player.setVelocityX(-300);
+        this.direction=-1;
         this.player.setFlipX(true);
         if (this.player.body.onFloor()) {
             //this.player.play('right', true)
