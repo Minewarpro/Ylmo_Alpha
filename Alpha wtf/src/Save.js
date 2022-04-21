cad4bf390a715b82977ae1834d10bc265dc09735
Minewarpro <hugo.roche13@hotmail.com>
2022-04-22 00:47:57 +0200

    death mieux

--- Alpha wtf/src/Save.js
+++ Alpha wtf/src/Save.js
@@ -19,9 +19,19 @@ class Save {
     }
 
     death(){
+        let me = this;
+        this.player.fireBall.emitParticleAt(this.player.player.body.x, this.player.player.body.y);
         this.player.player.setVelocity(0, 0);
+        this.player.player.visible = false;
         this.player.player.body.x = this.currentSaveX;
         this.player.player.body.y = this.currentSaveY;
+        this.scene.cameras.main.stopFollow();
+        this.scene.light.setColor(0x000000);
+        setTimeout(function (){
+            me.player.player.visible = true;
+            me.scene.cameras.main.startFollow(me.player.player,false,1,1,0,150);
+            me.scene.light.setColor(0x0f6fbf);
+        },500);
     }
     sauvegarde(player, saves) {
         this.currentSaveX = player.body.x
